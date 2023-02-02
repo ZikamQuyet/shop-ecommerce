1d37163c18779c2c9a02e1f52c6fc8cc3d548347
quyetnm12 <quyetnm@technixo.com>
2023-02-02 11:14:46 +0700

    update ui collections

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,7 +1,54 @@
 import React from 'react'
+import { Box, Typography, Stack, Button, Collapse, useMediaQuery } from '@mui/material'
+import { Link } from 'react-router-dom'
+import useToggle from '../hooks/useToggle'
 
 const Card = () => {
-  return <></>
+  const [open, handleToggle] = useToggle()
+  const matches900 = useMediaQuery('(min-width:900px)')
+  return (
+    <>
+      <Box position={'relative'} onMouseEnter={handleToggle} onMouseLeave={handleToggle}>
+        <Link to='/product'>
+          <img src='https://bizweb.dktcdn.net/thumb/large/100/414/728/products/13-3.jpg?v=1672389512000' alt='' />
+        </Link>
+        <Link to='/product'>
+          <Typography variant='h5' p={'0.5rem 0'}>
+            san pham 1
+          </Typography>
+        </Link>
+        <Stack direction={'row'} gap={2} alignItems='flex-end'>
+          <Typography fontSize='14px'>359000 vnd</Typography>
+          <Typography fontSize='12px' fontStyle={'initial'} color='#ccc' sx={{ textDecoration: 'line-through' }}>
+            399000 vnd
+          </Typography>
+        </Stack>
+        {matches900 ? (
+          <Collapse
+            in={open}
+            timeout='auto'
+            unmountOnExit
+            sx={{
+              position: 'absolute',
+              bottom: 75,
+              left: 0,
+              right: 0
+            }}
+          >
+            <Stack direction={'row'} justifyContent='space-between'>
+              <Button variant='outlined'>Thêm giỏ hàng</Button>
+              <Button variant='contained'>Mua ngay</Button>
+            </Stack>
+          </Collapse>
+        ) : (
+          <Stack direction={'row'} justifyContent='space-between' marginTop={1}>
+            <Button variant='outlined'>Thêm giỏ hàng</Button>
+            <Button variant='contained'>Mua ngay</Button>
+          </Stack>
+        )}
+      </Box>
+    </>
+  )
 }
 
 export default Card
--- src/pages/Collections.tsx
+++ src/pages/Collections.tsx
@@ -1,27 +1,40 @@
-import { Box, Collapse, Container, Grid, List, ListItem, Stack, Typography, useMediaQuery } from '@mui/material'
+import {
+  Box,
+  Collapse,
+  Container,
+  Grid,
+  List,
+  ListItem,
+  Pagination,
+  Stack,
+  Typography,
+  useMediaQuery
+} from '@mui/material'
 import { Link } from 'react-router-dom'
 import ExpandLess from '@mui/icons-material/ExpandLess'
 import ExpandMore from '@mui/icons-material/ExpandMore'
 import useToggle from '../hooks/useToggle'
+import Card from '../components/Card'
 
 const Collections = () => {
-  const [open, handleToggle] = useToggle()
+  const [openCategory, handleToggleCategory] = useToggle()
+  const [openOther, handleToggleOther] = useToggle()
   const matches900 = useMediaQuery('(min-width:900px)')
   return (
-    <Box m={matches900 ? '8rem 0' : '4rem 0'}>
+    <Stack marginTop={matches900 ? '8rem' : '4rem'}>
       <Stack direction={'row'} justifyContent='space-between'>
         <Box
-          onMouseEnter={handleToggle}
-          onMouseLeave={handleToggle}
+          onMouseEnter={handleToggleCategory}
+          onMouseLeave={handleToggleCategory}
           sx={{ display: 'flex', flexDirection: 'column', position: 'relative' }}
         >
           <Stack direction='row' justifyContent={'space-between'} p={2}>
             <Typography>Danh mục</Typography>
-            {open ? <ExpandLess /> : <ExpandMore />}
+            {openCategory ? <ExpandLess /> : <ExpandMore />}
           </Stack>
 
           <Collapse
-            in={open}
+            in={openCategory}
             timeout='auto'
             unmountOnExit
             sx={{
@@ -31,7 +44,8 @@ const Collections = () => {
               width: `${matches900 ? 'unset' : '100vw'}`,
               bgcolor: '#fff',
               boxShadow: 5,
-              borderRadius: 1
+              borderRadius: 1,
+              zIndex: '99'
             }}
           >
             <List component='div' disablePadding>
@@ -53,12 +67,56 @@ const Collections = () => {
             </List>
           </Collapse>
         </Box>
-        <Box p={2}>Tìm theo</Box>
+        <Box sx={{ display: 'flex', flexDirection: 'column', position: 'relative' }}>
+          <Stack
+            direction='row'
+            justifyContent={'space-between'}
+            p={2}
+            sx={{ cursor: 'pointer' }}
+            onClick={handleToggleOther}
+          >
+            <Typography>Tìm theo</Typography>
+            {openOther ? <ExpandLess /> : <ExpandMore />}
+          </Stack>
+
+          <Collapse
+            in={openOther}
+            timeout='auto'
+            unmountOnExit
+            sx={{
+              position: 'absolute',
+              top: 40,
+              right: `${matches900 ? '16px' : '0px'}`,
+              width: `${matches900 ? 'unset' : '100vw'}`,
+              bgcolor: '#fff',
+              boxShadow: 5,
+              borderRadius: 1,
+              zIndex: '99'
+            }}
+          >
+            <Box p={1}>chưa có chức năng này</Box>
+          </Collapse>
+        </Box>
       </Stack>
-      <Grid container p={1}>
-        <Grid item xs={6} md={4} lg={3}></Grid>
+      <Grid container p={2} spacing={5}>
+        <Grid item xs={6} md={4} xl={3}>
+          <Card />
+        </Grid>
+        <Grid item xs={6} md={4} xl={3}>
+          <Card />
+        </Grid>
+        <Grid item xs={6} md={4} xl={3}>
+          <Card />
+        </Grid>
+        <Grid item xs={6} md={4} xl={3}>
+          <Card />
+        </Grid>
       </Grid>
-    </Box>
+
+      <Stack spacing={2} m='2rem auto'>
+        <Pagination count={10} shape='rounded' />
+      </Stack>
+    </Stack>
   )
 }
 
